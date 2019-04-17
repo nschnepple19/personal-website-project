3fbab7fe8144580f441b1f4eff26281d6935293c
NickSchnepple <nschnepple@cnm.edu>
2019-04-17 15:42:27 -0600

    recreated my audience purpose and goal statements

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,15 +7,15 @@
 
 <body>
 
-		<h2>Purpose, Audience, Goal</h2>
+	<h2>Purpose, Audience, Goal</h2>
 
-			<p><strong>Purpose:</strong> This website will be a professional
-			portfolio to show off to possible employers.</p>
-
-			<p><strong>Audience:</strong>  The audience intended for this website will mainly be for
-			Employers and Customers who need sites built for their company or personal sites.</p>
-
-			<p><strong>Goal:</strong> Goal: The goal of this site will be to acquire a full-time
-				long-term job with my uncle and his fellow employees at TriCore.</p>
+	<ul>
+		<li><strong>Purpose:</strong> This website will be a professional portfolio to
+			show off to possible employers.</li>
+		<li><strong>Audience:</strong> The audience intended for this website will mainly
+			be for Employers and Customers who need sites built for their company or personal sites.</li>
+		<li><strong>Goal:</strong> The goal of this site will be to acquire a full-time long-term job
+			with my uncle and his fellow employees at TriCore.</li>
+	</ul>
 
 </body>
