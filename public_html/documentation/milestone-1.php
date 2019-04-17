1038d1d847bd626b1de54dbd747f4a6d01ec7c87
NickSchnepple <nschnepple@cnm.edu>
2019-04-17 15:35:50 -0600

    I created the Purpose, Audience, and Goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,8 +9,13 @@
 
 		<h2>Purpose, Audience, Goal</h2>
 
-		<ul>
-			<li><strong>Purpose:</strong></li>
-		</ul>
+			<p><strong>Purpose:</strong> This website will be a professional
+			portfolio to show off to possible employers.</p>
+
+			<p><strong>Audience:</strong>  The audience intended for this website will mainly be for
+			Employers and Customers who need sites built for their company or personal sites.</p>
+
+			<p><strong>Goal:</strong> Goal: The goal of this site will be to acquire a full-time
+				long-term job with my uncle and his fellow employees at TriCore.</p>
 
 </body>
