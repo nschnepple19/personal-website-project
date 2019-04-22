b70e39ccf1eba557b5e74ff2b0f8563b05bb29a6
NickSchnepple <nschnepple@cnm.edu>
2019-04-22 11:30:50 -0600

    added milestone 2 and set up the html tags for it.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone Two</title>
+	</head>
+
+	<body>
+		<h1>Content Strategy</h1>
+
+		<p>My site will be one page, and will consist of the following
+		sections.</p>
+
+	</body>
